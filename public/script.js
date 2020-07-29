e51ff29fa13f950e53cebfc7ea3fb6a8d48bc360
phong  nguyen <phongit1995@gmail.com>
2020-07-29 17:01:03 +0700

    fix api

--- public/script.js
+++ public/script.js
@@ -2,7 +2,8 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: 'webpeerserver.herokuapp.com',
-  port:433
+  port:80,
+  source:true
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
