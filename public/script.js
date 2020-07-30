9f0f2c314caa7706088a53b63cb3ca77b80468ac
phong  nguyen <phongit1995@gmail.com>
2020-07-30 14:19:45 +0700

    fix ssl

--- public/script.js
+++ public/script.js
@@ -2,7 +2,8 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: '52.231.160.98',
-  port:5555
+  port:5555,
+  secure:true
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
